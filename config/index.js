4944f02898a8136772d05dfd5d2ddce8f8e5d401
MinJia <aa813240@gmail.com>
2020-11-03 18:45:34 +0800

    index修改

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Fairy-tree/',
     proxyTable: {},
 
     // Various Dev Server settings
